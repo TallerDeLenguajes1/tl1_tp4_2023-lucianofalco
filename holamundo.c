892dd575d24c9687b1e505e65e42fc2f808c30b3
Luciano <lucianosebastianfalco@gmail.com>
2023-04-20 19:19:07 -0300

    punto 2_6

---
+++ holamundo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int a ; 
+    printf("Ingrese algo\n");
+    scanf("%d" , &a);
+
+    printf("Se ingreso %d\n" , a );
+    return 0;
+}
--- punto2.c
+++ punto2.c
@@ -12,7 +12,9 @@ int Duracion; // entre 10 – 100
 int main(void)
 {
     int cantidad , i  ; 
-    Tarea ** tareas ;
+        int contador ; 
+    Tarea ** tareaspendientes ;
+    Tarea **tareasRealizadas;
     char *buff ;
 
     buff=(char*)malloc(sizeof(char)*100) ;
@@ -22,10 +24,14 @@ int main(void)
         scanf("%d" , &cantidad);
     } while (cantidad < 1);
 
-    tareas = (Tarea**) malloc(sizeof(Tarea*)*cantidad);
+    tareaspendientes = (Tarea**) malloc(sizeof(Tarea*)*cantidad);
+    tareasRealizadas = (Tarea**) malloc(sizeof(Tarea*)*cantidad);
+    
 
     for ( i = 0; i < cantidad; i++) {
-        tareas[i] = (Tarea*)malloc(sizeof(Tarea));
+        tareaspendientes[i]=NULL;
+        tareasRealizadas[i]=NULL;
+        tareaspendientes[i] = (Tarea*)malloc(sizeof(Tarea));
     }
     /*Desarrolle una interfaz de consola para cargar las tareas, ingresando su
 descripción y duración. Recuerde utilizar reserva de memoria dinámica para la
@@ -33,26 +39,64 @@ carga de la descripción.*/
     
     for ( i = 0; i < cantidad; i++)
     {
-        tareas[i]->TareaID = i + 1 ;
+        tareaspendientes[i]->TareaID = i + 1 ;
         printf("\nIngrese la tarea #%i\n" , i+1);
         fflush(stdin);
         gets(buff);
-        tareas[i]->Descripcion = (char*)malloc(strlen(buff)+1);
-        strcpy(tareas[i]->Descripcion , buff);
+        tareaspendientes[i]->Descripcion = (char*)malloc(strlen(buff)+1);
+        strcpy(tareaspendientes[i]->Descripcion , buff);
 
         printf("Ingrese la duracion \n");
         fflush(stdin);
-        scanf("%d", &tareas[i]->Duracion);
-        printf("nuevos cambios");
+        scanf("%d", &tareaspendientes[i]->Duracion);
     }
     
-    
-    
+    /*4. Una vez cargada todas las tareas. Irá listando de a una las tareas y
+preguntando si se realizó o no la misma. Si la respuesta es Si tiene que
+“mover” dicha tarea a otro arreglo denominado tareas realizadas que tendrá el
+mismo tamaño que el anterior. Una vez movida la tarea ese casillero del vector
+tiene que apuntar a null. Como se muestra en la gráfica a continuación.*/
+    contador = 0 ;
 
+    for ( i = 0; i < cantidad; i++)
+    {
+        int desicion ; 
+        printf("\nPresione 1) si realizo la area y presione 2) si no realizo aun la tarea , '%s'\n", tareaspendientes[i]->Descripcion);
+        printf("Duracion: %d\n",tareaspendientes[i]->Duracion);
+        scanf("%d" , &desicion);
+        if (desicion==1)
+        {
+            tareasRealizadas[i]=tareaspendientes[i];
+            tareaspendientes[i]=NULL;
+        }       
+    }
     
+    printf("\nTareas realizadas:\n");
+    for ( i = 0; i < cantidad ; i++)
+    {
+        if (tareasRealizadas[i] != NULL)
+        {
+            printf("Tarea #%i , %s\n" , i+1 ,tareasRealizadas[i]->Descripcion);
+            contador = contador+1 ; 
+        }
+    }
+    printf("\nLas tareas realizadas son: %i\n" , contador);
     
+    printf("\ntareas pendientes");
+    contador = 0 ;
 
-
+    for (i = 0; i < cantidad; i++)
+    {
+        if (tareaspendientes[i] != NULL)
+        {
+            printf("Tarea #%i , %s\n" , i+1 ,tareaspendientes[i]->Descripcion);
+            contador = contador+1 ; 
+        }
+        
+        printf("Tarea #%i , %s\n" , i+1 ,tareaspendientes[i]->Descripcion);
+        contador = contador+1 ;     
+    }
+    printf("\nLas tareas realizadas son  %d \n " , contador);
     return 0;
 }
 
